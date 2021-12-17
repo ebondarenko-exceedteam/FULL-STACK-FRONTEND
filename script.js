f749ecee6400d8eac6559d0b12291f4d877b9876
Evgeny Bondarenko <ebondarenko.exceedteam@gmail.com>
2021-12-17 15:36:29 +0300

    added change for date

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ let today = new Date();
 const dd = String(today.getDate()).padStart(2, '0');
 const mm = String(today.getMonth() + 1).padStart(2, '0');
 const yyyy = today.getFullYear();
-today = `${dd}.${mm}.${yyyy}`;
+today = `${yyyy}-${mm}-${dd}`;
 
 window.onload = async () => {
   inputWhere = document.getElementById('input_where');
   inputHowMany = document.getElementById('input_how_many');
   const button = document.querySelector('button');
   inputWhere.addEventListener('change', changeInputWhere);
-  inputHowMany.addEventListener('change', changeinputHowMany);
+  inputHowMany.addEventListener('change', changeInputHowMany);
   button.addEventListener('click', addNewRecording);
   const resp = await fetch('http://localhost:8800/getAllCharges', { method: 'GET'});
   const fetchResult = await resp.json();
@@ -28,7 +28,11 @@ const changeInputWhere = (event) => {
   inputWhereValue = event.target.value;
 }
 
-const changeinputHowMany = (event) => {
+const changeInputDate = (event) => {
+  inputDateValue = event.target.value;
+}
+
+const changeInputHowMany = (event) => {
   inputHowManyValue = event.target.value;
 }
 
@@ -48,13 +52,14 @@ const addNewRecording = async () => {
   const fetchResult = await resp.json();
   recordingsArray = fetchResult.data;
   resultSum += +inputHowManyValue;
+  inputDateValue = today;
   inputWhere.value = '';
   inputHowMany.value = '';
   render();
 };
 
 const changeRecording = async (index) => {
-  const { _id, howMany } = recordingsArray[index];
+  const { _id, date, where, howMany } = recordingsArray[index];
   const resp = await fetch ('http://localhost:8800/updateCharges', {
     method: 'PATCH',
     headers: {
@@ -63,6 +68,7 @@ const changeRecording = async (index) => {
 		},
     body: JSON.stringify({
       _id,
+      date: inputDateValue,
       where: inputWhereValue,
       howMany: inputHowManyValue
     })
@@ -71,20 +77,22 @@ const changeRecording = async (index) => {
   recordingsArray = fetchResult.data;
   resultSum -= Number(howMany);
   resultSum += +inputHowManyValue;
-  inputWhere.value = '';
-  inputHowMany.value = '';
+  inputWherevalue = '';
+  inputHowManyvalue = '';
+  inputDateValue = '';
   render();
 };
 
 const closeRecording = () => {
-  inputWhereValue = '';
-  inputHowManyValue = '';
   render();
 };
 
 const editRecording = (index) => {
-  const { where, howMany } = recordingsArray[index];
-  const activeRecording = document.getElementById(`record_${index}`)
+  const { date, where, howMany } = recordingsArray[index];
+  inputWhereValue = where;
+  inputHowManyValue = howMany;
+  inputDateValue = date;
+  const activeRecording = document.getElementById(`record_${index}`);
   while (activeRecording.firstChild) {
     activeRecording.removeChild(activeRecording.firstChild);
   };
@@ -93,11 +101,18 @@ const editRecording = (index) => {
   editInputWhere.type = 'text';
   editInputWhere.value = where;
   editInputWhere.addEventListener('change', changeInputWhere);
+
+  const editInputDate = document.createElement('input');
+  editInputDate.id = 'input_edit_date';
+  editInputDate.type = 'date';
+  editInputDate.value = date;
+  editInputDate.addEventListener('change', changeInputDate);
+
   const editInputHowMany = document.createElement('input');
   editInputHowMany.id = 'input_edit_how_many';
   editInputHowMany.type = 'number';
   editInputHowMany.value = howMany;
-  editInputHowMany.addEventListener('change', changeinputHowMany);
+  editInputHowMany.addEventListener('change', changeInputHowMany);
   const editBtnDiv = document.createElement('div');
   editBtnDiv.className = 'edit_btn_div';
   const editBtnEdit = document.createElement('img');
@@ -113,15 +128,18 @@ const editRecording = (index) => {
   editBtnDiv.appendChild(editBtnEdit);
   editBtnDiv.appendChild(editBtnClose);
   activeRecording.appendChild(editInputWhere);
+  activeRecording.appendChild(editInputDate);
   activeRecording.appendChild(editInputHowMany);
   activeRecording.appendChild(editBtnDiv);
 };
+
 const deleteRecording = async (index) => {
   const resp = await fetch(`http://localhost:8800/deleteCharges?_id=${recordingsArray[index]._id}`, { method: 'DELETE'});
   const fetchResult = await resp.json();
   recordingsArray = fetchResult.data;
   render();
 };
+
 const newChangeInputWhere = async (event, index) => {
   inputWhereValue = event.target.value;
   const resp = await fetch ('http://localhost:8800/updateCharges', {
@@ -139,6 +157,7 @@ const newChangeInputWhere = async (event, index) => {
   recordingsArray = fetchResult.data;
   render();
 };
+
 const newChangeInputDate = async (event, index) => {
   inputDateValue = event.target.value;
   const resp = await fetch ('http://localhost:8800/updateCharges', {
@@ -156,30 +175,37 @@ const newChangeInputDate = async (event, index) => {
   recordingsArray = fetchResult.data;
   render();
 };
+
 const editTitleWhere = (index) => {
   const recordingDiv = document.getElementById(`record_${index}`);
-  const recordTitleWhere = recordingDiv.firstChild;
+  let recordTitleWhere = recordingDiv.firstChild;
   const newInputEditWhere = document.createElement('input');
   newInputEditWhere.id = 'new_input_edit_where';
   newInputEditWhere.type = 'text';
   newInputEditWhere.value = recordingsArray[index].where;
   recordTitleWhere = recordingDiv.replaceChild(newInputEditWhere, recordTitleWhere);
-  newInputEditWhere.addEventListener('blur', () => {
+  newInputEditWhere.addEventListener('blur', (event) => {
     newChangeInputWhere(event, index);
   });
 };
+
 const editTitleDate = (index) => {
   const recordTitleBox = document.getElementById(`title_box_${index}`);
-  const recordTitleDate = recordTitleBox.firstChild;
+  let recordTitleDate = recordTitleBox.firstChild;
   const newInputEditDate = document.createElement('input');
   newInputEditDate.id = 'new_input_edit_date';
-  newInputEditDate.id = 'new_input_edit_date';
+  newInputEditDate.type = 'date';
   newInputEditDate.value = recordingsArray[index].date;
   recordTitleDate = recordTitleBox.replaceChild(newInputEditDate, recordTitleDate);
   newInputEditDate.addEventListener('blur', () => {
     newChangeInputDate(event, index);
   });
 };
+
+const changeDateText = (date) => {
+  return date.split('-').reverse().join('-');
+}
+
 const render = () => {
   const resultBlock = document.querySelector('.result_block');
   while (resultBlock.firstChild) {
@@ -211,7 +237,7 @@ const render = () => {
     recordTitleBox.className = 'record_title_box';
     recordTitleBox.id = `title_box_${index}`;
     const recordTitleDate = document.createElement('p');
-    recordTitleDate.innerText = date;
+    recordTitleDate.innerText = changeDateText(date);
     recordTitleDate.className = 'record_title_date';
     recordTitleDate.addEventListener('dblclick', () => {
       editTitleDate(index);
